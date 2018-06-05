71ee5bfef38ff0614b60b993bbc6cf29b72e63e5
luoxud <35617039+luoxud@users.noreply.github.com>
2018-06-05 14:58:43 +0800

    update

--- sql.js
+++ sql.js
@@ -1,11 +1,9 @@
 const Mysql = require('mysql');
 // 创建连接
 const connection = Mysql.createConnection({
-  // host: '172.31.118.139',
   host: 'localhost',
   user: 'root',
   port: 3306,
-  // password: 'Mysql1996!',
   password: 'mysql1996',
   database: 'chat',
   useConnectionPooling: true
